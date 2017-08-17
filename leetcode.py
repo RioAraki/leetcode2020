657f87bea9f96d86999fb080ebd42584e750124a
RioAraki <yueee.li@mail.utoronto.ca>
2017-08-16 23:22:25 -0400

    more research on 658. Find K Closest Elements

--- leetcode.py
+++ leetcode.py
@@ -1,4 +1,7 @@
 # https://leetcode.com/contest/leetcode-weekly-contest-45/problems/judge-route-circle/
+# Thoughts: set the initial position to 0, keep track of the current position, if current position after move finished
+# is 0, then its a circle
+
 
 def judgeCircle(self, moves):
     """
@@ -36,34 +39,43 @@ def judgeCircle(self, moves):
 
 ############################################################################################
 # https://leetcode.com/contest/leetcode-weekly-contest-45/problems/find-k-closest-elements/
-    def findClosestElements(self, arr, k, x):
-        """
-        :type arr: List[int]
-        :type k: int
-        :type x: int
-        :rtype: List[int]
-        """
-        diffList = self.listModify(arr, x)
-        result_list = []
 
-        # lambda ->
-        # diffList = diffList.sort(key= lambda tup: tup[0])
-        diffList.sort(key=lambda tup: tup[0])
+# build a new list which records the abs diff of each element and x, also keep track of it's original value and position     O(n)
+# sort through its abs diff value     O(log(n))
+# pick the first kth value, choose original value which forms a new list            O(n)
+# sort the list    O(log(n))
 
-        for num in range(k):
-            result_list.append(diffList[num][2])
-        result_list.sort()
+def findClosestElements(self, arr, k, x):
+    """
+    :type arr: List[int]
+    :type k: int
+    :type x: int
+    :rtype: List[int]
+    """
+    diffList = self.listModify(arr, x)
+    result_list = []
 
-        return result_list
+    # lambda ->
+    # diffList = diffList.sort(key= lambda tup: tup[0])
+    diffList.sort(key=lambda tup: tup[0])
 
-    def listModify(self, arr, x):
-        result = []
-        smallest = 999999999999
-        curr_index = 0
-        for element in arr:
-            new_ele = abs(x - element)
-            result.append((new_ele, curr_index, element))
-            if new_ele < smallest:
-                smallest = new_ele
-            curr_index += 1
-        return result
+    for num in range(k):
+        result_list.append(diffList[num][2])
+    result_list.sort()
+
+    return result_list
+
+def listModify(self, arr, x):
+    result = []
+    smallest = 999999999999
+    curr_index = 0
+    for element in arr:
+        new_ele = abs(x - element)
+        result.append((new_ele, curr_index, element))
+        if new_ele < smallest:
+            smallest = new_ele
+        curr_index += 1
+    return result
+
+#TODO: Use the way in http://bookshadow.com/weblog/2017/08/13/leetcode-find-k-closest-elements/ and resolve the problem!
+############################################################################################
