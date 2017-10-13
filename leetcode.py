02d01dbdb25b5555b5d2d6255df22674c3f22814
RioAraki <yueee.li@mail.utoronto.ca>
2017-10-12 22:47:10 -0400

    add question and solution for max area

--- leetcode.py
+++ leetcode.py
@@ -989,3 +989,20 @@ def repeatedStringMatch(self, A, B):
         bits = bin(n)
         return all(bits[i] != bits[i + 1]
                    for i in xrange(len(bits) - 1))
+
+############################################################################################
+# https://leetcode.com/articles/max-area-of-island/
+    def maxAreaOfIsland(self, grid):
+        seen = set()
+
+        def area(r, c):
+            if not (0 <= r < len(grid) and 0 <= c < len(grid[0])
+                    and (r, c) not in seen and grid[r][c]):
+                return 0
+            seen.add((r, c))
+            return (1 + area(r + 1, c) + area(r - 1, c) +
+                    area(r, c - 1) + area(r, c + 1))
+
+        return max(area(r, c)
+                   for r in range(len(grid))
+                   for c in range(len(grid[0])))
