ae78b10dd6ec5f6dcba6b6e9ca6873daa636b289
RioAraki <yueee.li@mail.utoronto.ca>
2018-04-18 00:27:09 -0400

    manage to implement a generic stack in c++

--- CLRS/CLRS_implementation/CLRS_implementation/implementation.cpp
+++ CLRS/CLRS_implementation/CLRS_implementation/implementation.cpp
@@ -1,50 +1,55 @@
-
-
+#include <iostream>
+#include <vector>
 // stack implementation, easiest way, no dynamic, no pointer
 //# define MAX 1000;
 
-
-class Stack {
-	int top;
-	const static int MAX = 1000;
-public:
-	int array[MAX];
-	
-	Stack() {
-		top = -1;
-	};
-
-	bool push(int x);
-	int pop();
-	bool isEmpty();
-};
-
-bool Stack::push(int x) {
-	if (top >= MAX) {
-		return false;
-	} else {
-		array[++top] = x;
-		return true;
-	}
-}
-
-int Stack::pop() {
-	if (top == -1) {
-		return 0; // what if we mess up with 0 stored in stack?
-	}
-	else {
-		int x = array[top--];
-		return x;
-	}
-}
-
-bool Stack::isEmpty() {
-	return top == -1;
-}
-
-
 // dynamic, generic
+template<typename T>
 class better_stack {
+private:
+	int m_top;
+	const int MAX = 1000;
+	std::vector<T> m_Array;
+public:
+	better_stack() {
+		m_top = -1;
+	}
+	bool push(T x) {
+		if (m_top == MAX) {
+			return false;
+		}
+		else {
+			m_Array.push_back(x);
+			++m_top;
+			return true;
+		}
+	}
 
+	T pop() {
+		if (m_top < 0) {
+			return false;
+		}
+		else {
+			T result = m_Array.back();
+			m_Array.pop_back();
+			return result;
 
+		}
+	}
+	bool isEmpty() {
+		return (m_top == -1);
+	}
 };
+
+int main() {
+	better_stack<char> stack;
+	std::cout << stack.push('a') << std::endl;
+	std::cout << stack.push('b') << std::endl;
+	std::cout << stack.pop() << std::endl;
+	std::cout << stack.pop() << std::endl;
+	std::cout << stack.isEmpty() << std::endl;
+	std::cin.get();
+
+}
+
+
