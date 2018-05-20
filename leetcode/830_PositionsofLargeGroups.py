dc89ba9067945dc71eb58eb521ae60a0b2d39d71
RioAraki <yueee.li@mail.utoronto.ca>
2018-05-20 16:34:34 -0400

    done 830

---
+++ leetcode/830_PositionsofLargeGroups.py
@@ -0,0 +1,24 @@
+class Solution:
+    def largeGroupPositions(self, S):
+        """
+        :type S: str
+        :rtype: List[List[int]]
+        """
+
+        ret = []
+        beg = 0
+        cur = S[0]
+
+        for i in range(len(S)):
+            if cur != S[i]:
+                if i - beg >= 3:  # error 1: misuse of cur, should be beg
+                    ret.append([beg, i - 1])
+                cur = S[i]
+                beg = i
+
+        # error2: forget to check tail
+        # error3: forget to use len - 1 to represent last one in list
+        if (len(S) - 1) - beg >= 2:
+            ret.append([beg, len(S) - 1])
+
+        return ret
