8e40eb271a449f23b2f11ecea792892d3fae6928
RioAraki <yueee.li@mail.utoronto.ca>
2018-05-29 12:05:14 -0400

    addq843

---
+++ leetcode/Q843_GuesstheWord.py
@@ -0,0 +1,20 @@
+# """
+# This is Master's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class Master(object):
+#    def guess(self, word):
+#        """
+#        :type word: str
+#        :rtype int
+#        """
+
+class Solution(object):
+    def findSecretWord(self, wordlist, master):
+        """
+        :type wordlist: List[Str]
+        :type master: Master
+        :rtype: None
+        """
+
+        # Unable to find a universal guaranteed solution, more about a strategy
