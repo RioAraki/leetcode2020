7ba4d8a9db353465ea77620fce8267f81da4ab48
RioAraki <yueee.li@mail.utoronto.ca>
2018-05-29 01:02:44 -0400

    finish 842 but tle

--- leetcode/Q842_SplitArrayintoFibonacciSequence.py
+++ leetcode/Q842_SplitArrayintoFibonacciSequence.py
@@ -6,10 +6,9 @@ class Solution(object):
         """
         # The first two elements are free to choose, and dont have any limitation
         # The first two elements consumes at most 2/3 spaces in the original string
-
-        # corner case
-        if S[0] == 0:
-            return []
+        trigger = 0  # trigger = 0 if start with 0
+        if int(S[0]) == 0:
+            trigger = 1
 
         ret = []
         end = int(round(len(S) * 2 / 3))
@@ -18,39 +17,47 @@ class Solution(object):
             for j in range(i + 1, end + 1):
                 fib1, fib2 = int(S[0:i]), int(S[i:j])
                 ret = [fib1, fib2]
-                print("fib1: " + str(fib1) + ", fib2: " + str(fib2))
+
                 k = j + 1
                 while k < len(S) + 1:
-                    print("fib3: " + S[j:k])
+
                     # last element check
                     if k == len(S):
-                        print("last one, fib1: ", str(fib1), " ,fib2: ", str(fib2), " ,fib3:", S[j:k])
+
                         if fib1 + fib2 != int(S[j:k]):
                             break
                         else:
+                            if int(S[j:k]) > 2 ** 31 - 1:
+                                break
                             ret.append(int(S[j:k]))
+                            if trigger == 1 and ret[0] != 0:
+                                return []
                             return ret
 
-                    # find the third element, update j,k,ret and perform while loop recursively
                     if int(S[j:k]) == fib1 + fib2:
-                        print('fib3 == fib1+2')
+                        # print('fib3 == fib1+2')
                         ret.append(int(S[j:k]))
                         fib1 = fib2
                         fib2 = int(S[j:k])
                         j = k
                         k += 1
-                    # current third element too small
+
                     elif int(S[j:k]) < fib1 + fib2:
-                        print('fib3 < fib1+2')
+                        # print('fib3 < fib1+2')
                         k += 1
 
-                    # unable to find the third element, end the while loop and find new first and second fib number
+
                     elif int(S[j:k]) > fib1 + fib2:
-                        print('fib3 > fib1+2')
+                        # print('fib3 > fib1+2')
                         ret = []
                         break
 
         return []
 
         # Error 1: index error: range's end index does not count
-        # Error 2: type error: forget to change type in S[j:k]
+        # Error 2: type error: forget to change type in S[j:k], i know we need to do the transit, but still miss it in a lot of places
+        # Error 3: misunderstand/ missing lines: i thought "leading 0 not allowed" means list start with 0 is not allowed. It actually means cannot interpret [0,1] as 01
+        # Error 4: misunderstand/ missing lines:0 <= F[i] <= 2^31 - 1
+        # Error 5: TLE
+
+
