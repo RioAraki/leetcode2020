6a5720e0ee217ef2b92d68c30bf1c31462ba9a95
RioAraki <yueee.li@mail.utoronto.ca>
2018-06-01 16:53:11 -0400

    finish 95

---
+++ leetcode/95_UniqueBinarySearchTreesII.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def generateTrees(self, n):
+        """
+        :type n: int
+        :rtype: List[TreeNode]
+        """
+
+        if n == 0:
+            return []
+        return self.dfs(1, n + 1)
+
+    def dfs(self, start, end):
+        if start == end:
+            return None
+        result = []
+        for i in range(start, end):
+            for l in self.dfs(start, i) or [None]:
+                for r in self.dfs(i + 1, end) or [None]:
+                    node = TreeNode(i)
+                    node.left, node.right = l, r
+
+                    result.append(node)
+
+                    print(result)
+        return result
+
+
+
+# Not done by myself, need to review again and redo.
