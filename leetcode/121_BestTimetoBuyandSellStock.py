b83c8d5278a5f3a0865d72cb0e27b45669dd957c
RioAraki <yueee.li@mail.utoronto.ca>
2018-06-11 17:32:41 -0400

    finish 121

---
+++ leetcode/121_BestTimetoBuyandSellStock.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if len(prices) <= 1:
+            return 0
+
+        minprice = prices[0]
+        maxpro = 0
+
+        for i in range(1, len(prices)):
+            minprice = min(minprice, prices[i])
+            maxpro = max(maxpro, max(0, prices[i] - minprice))
+
+        return maxpro 
