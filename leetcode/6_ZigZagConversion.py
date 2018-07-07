9540ff1fa28f335ac3a3bc2fd27dab5afbe837a4
RioAraki <yueee.li@mail.utoronto.ca>
2018-07-07 11:25:01 +0800

    add my solution for 6, but tle

---
+++ leetcode/6_ZigZagConversion.py
@@ -0,0 +1,32 @@
+# My solution but TLE
+
+
+def convert(self, s, numRows):
+    """
+    :type s: str
+    :type numRows: int
+    :rtype: str
+    """
+    # num of letters per zig
+    zig = numRows * 2 - 2
+    ret = ""
+
+    # loop through each row
+    for i in range(numRows):
+        row = i
+        # if first or last row, each round add one zig
+        if i == 0 or i == numRows - 1:
+            while i < len(s):
+                ret += s[i]
+                i += zig
+        # else each round add one zig and one zig - 2*rows
+        else:
+            while i < len(s):
+                ret += s[i]
+                i += (zig - 2 * row)
+                if i < len(s):
+                    ret += s[i]
+                    i += 2 * row
+
+    return ret
+
