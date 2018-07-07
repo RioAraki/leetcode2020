62285e8955dea86a45316b45b0f9dc6ffef513e2
RioAraki <yueee.li@mail.utoronto.ca>
2018-07-07 10:42:54 +0800

    add my solution for 5

--- leetcode/5_LongestPalindromicSubstring.py
+++ leetcode/5_LongestPalindromicSubstring.py
@@ -1,4 +1,32 @@
+# my solution
 
+def longestPalindrome(self, s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    if len(s) == 0:
+        return ""
+
+    ret = s[0]
+
+    # expand from center algorithm
+
+    for i in range(len(s)):
+        for j in [i, i + 1]:
+            if j < len(s):
+                counter = 1
+                if s[i] == s[j]:
+                    if len(s[i:j + 1]) > len(ret):
+                        ret = s[i:j + 1]
+                    while i - counter >= 0 and j + counter < len(s):
+                        if s[i - counter] == s[j + counter]:
+                            if len(s[i - counter:j + counter + 1]) > len(ret):
+                                ret = s[i - counter:j + counter + 1]
+                            counter += 1
+                        else:
+                            break
+    return ret
 
 # better solution with the same algorithm
 def longestPalindrome(self, s):
