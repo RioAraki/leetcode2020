ff485d542b16ca2f0983349b0b10c90fc1e532a3
RioAraki <yueee.li@mail.utoronto.ca>
2018-07-15 14:10:19 +0800

    passing 17

--- leetcode/17_LetterCombinationsofaPhoneNumber.py
+++ leetcode/17_LetterCombinationsofaPhoneNumber.py
@@ -1,7 +1,4 @@
-# My failed solution. should be pretty close, only problem is that product(args*) requires multiple inputs but I dont know
-# how to feed such multiple parameters input to it, I can only from an one element list
-
-
+# mind the usage of *
 
 import itertools
 
@@ -12,7 +9,8 @@ class Solution:
         :type digits: str
         :rtype: List[str]
         """
+        if digits == "": return []
         dct = {"2": ["a", "b", "c"], "3": ["d", "e", "f"], "4": ["g", "h", "i"], "5": ["j", "k", "l"],
                "6": ["m", "n", "o"], "7": ["p", "q", "r", "s"], "8": ["t", "u", "v"], "9": ["w", "x", "y", "z"]}
 
-        return list(map(lambda x: "".join(x), itertools.product((dct[digits[i]] for i in range(len(digits))))))
+        return list(map(lambda x: "".join(x), itertools.product(*[dct[digits[i]] for i in range(len(digits))])))
