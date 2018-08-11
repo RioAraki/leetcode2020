03376d1ada3aad86549414f5c5738ef0c8503deb
RioAraki <yueee.li@mail.utoronto.ca>
2018-08-11 19:37:54 +0800

    more 878

--- leetcode/878. Nth Magical Number.py
+++ leetcode/878. Nth Magical Number.py
@@ -1,3 +1,9 @@
+# find out GCD (greatest common divisor) (a,b) = (b, a % b)
+# find out LCM (least common multiplier) A*B / gcd
+# find out how many magic numbers are there in each GCD range
+# by inclusion exclusion principle
+# find the magic numbers left
+
 def nthMagicalNumber(N, A, B):
     """
     :type N: int
@@ -30,6 +36,23 @@ def nthMagicalNumber(N, A, B):
 
 
     return ((N // sv) * s + rest(A, B, N % sv))%(10**9+7)
+
+
+# find gcd and lcm, but use binary search to find smallest x that
+# x / A + x / B - x / lcm = N
+def BetternthMagicalNumber(N, A, B):
+    a, b = A, B
+    # gcd
+    while b: a, b = b, a % b
+    l,r,lcm = 2, 10**14, A * B / a
+    while l < r:
+        m = (l+r)/2
+        # too small, increase least range
+        if m / A + m / B - m/ lcm < N: l = m+1
+        # too large, decrease largest range
+        else: r = m
+    # break the loop when l == r
+    return l % (10**9+7)
 
 
 if __name__ == "__main__":
