e6e21167354f7006888c7f39eab2190ca6dc8450
RioAraki <yueee.li@mail.utoronto.ca>
2018-08-15 14:22:22 +0800

    done 887

---
+++ leetcode/887_SuperEggDrop.py
@@ -0,0 +1,55 @@
+# Traditional and typical dp way to solve the issue
+# Get TLE
+
+class Solution:
+    def superEggDrop(self, N, K):
+        """
+        :type K: int
+        :type N: int
+        :rtype: int
+        """
+
+        # each row -> egg
+        # column -> floor
+        dp = [[0 for x in range(K + 1)] for x in range(N + 1)]
+
+        # set up initial, with 0 or 1 floor we only need to try 0 or 1 trials
+        for i in range(1, N + 1):
+            dp[i][1] = 1
+            dp[i][0] = 0
+
+        # with 1 egg and j floors always j trials
+        for j in range(1, K + 1):
+            dp[1][j] = j
+
+        # fill rest of the entries in table
+
+        for i in range(2, N + 1):
+            for j in range(2, K + 1):
+                dp[i][j] = 2 ** 31 - 1
+                for x in range(1, j + 1):
+                    ret = 1 + max(dp[i - 1][x - 1], dp[i][j - x])
+                    if ret < dp[i][j]:
+                        dp[i][j] = ret
+        return dp[N][K]
+
+
+# Use bottom up instead of top down, saves a lot of unnecessary entries filled in the 2d array.
+
+class Solution:
+    def superEggDrop(self, K, N):
+        """
+        :type K: int
+        :type N: int
+        :rtype: int
+        """
+        # 2d array, k -> egg; n -> num of moves; value ->
+        dp = [[0] * (K + 1) for i in range(N + 1)]
+
+        for m in range(1, N + 1):
+            for k in range(1, K + 1):
+                # if egg breaks, we can check dp[m-1][k-1] floors
+                # if egg does not break, we can check [m-1][k] floors
+
+                dp[m][k] = dp[m - 1][k - 1] + dp[m - 1][k] + 1
+            if dp[m][K] >= N: return m
