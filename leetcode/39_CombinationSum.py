b96012bfbc6bdfcf0cdf4f0eeb24662d8bb6bb1c
RioAraki <yueee.li@mail.utoronto.ca>
2018-08-18 22:35:45 +0800

    better 39, change dp method from top down to bottom up

--- leetcode/39_CombinationSum.py
+++ leetcode/39_CombinationSum.py
@@ -1,3 +1,5 @@
+# a top-down approach, might not be efficient enough since we go over every element in the saved data
+
 def combinationSum(candidates, target):
     """
     :type candidates: List[int]
@@ -20,3 +22,29 @@ def combinationSum(candidates, target):
     for i in range(1, target + 1):
         recurSum(i, dp)
     return dp[-1]
+
+# better bottom-up approach, more efficient
+
+def combinationSum(candidates, target):
+    """
+    :type candidates: List[int]
+    :type target: int
+    :rtype: List[List[int]]
+    """
+    # dp, saved data: 1d array, len(range(target)), each element reprs all solution sets to that index.
+    dp = [[] for i in range(target + 1)]
+
+    def recurSum(target, dp):
+        # base case
+        if target in candidates:
+            dp[target].append([target])
+        for can in candidates:
+            if target - can > 0:
+                if not len(dp[target - can]):
+                    dp[target - can] = recurSum(target - can, dp)
+                for comb in dp[target - can]:
+                    if sorted(comb + [can]) not in dp[target]:
+                        dp[target].append(sorted(comb + [can]))
+        return dp[target]
+
+    return recurSum(target, dp)
