fd6d4dea332053a8839348880352da6042326a50
RioAraki <yueee.li@mail.utoronto.ca>
2018-08-28 15:05:43 +0800

    add ac 895

--- leetcode/895_MaximumFrequencyStack.py
+++ leetcode/895_MaximumFrequencyStack.py
@@ -1,7 +1,31 @@
+import collections
+
+class FreqStack:
+    def __init___(self):
+        self.freq = collections.Counter()
+        self.m = collections.defaultdict(list)
+        self.maxf = 0
+
+    def push(self, x):
+        freq, m = self.freq, self.m
+        freq[x] += 1
+        self.maxf = max(self.maxf, freq[x])
+        m[freq[x]].append(x)
+
+    def pop(self):
+        freq, m, maxf = self.freq, self.m, self.maxf
+        x = m[maxf].pop()
+        # critical:
+        if not m[maxf]: self.maxf = maxf - 1
+        freq[x] -= 1
+        return x
+
+
 # correct but TLE
 
 # A dictionary to keep track of the num of elements in the stack
 # A list to keep track of the index of elements in the stack
+
 
 import operator
 class TLEFreqStack:
