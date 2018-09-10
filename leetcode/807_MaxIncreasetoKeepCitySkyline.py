5f338d3379f06557130185ce15a0be00a26109e9
RioAraki <yueee.li@mail.utoronto.ca>
2018-09-10 08:44:45 -0400

    better 807

--- leetcode/807_MaxIncreasetoKeepCitySkyline.py
+++ leetcode/807_MaxIncreasetoKeepCitySkyline.py
@@ -14,3 +14,11 @@ def maxIncreaseKeepingSkyline(self, grid):
         for j in range(len(grid[0])):
             if grid[i][j] < min(maxRow[i], maxCol[j]): ret += min(maxRow[i], maxCol[j]) - grid[i][j]
     return ret
+
+
+#  Use map to get row and col even quicker (the use of zip(*grid) is brilliant)
+#  Once we know row and col, no longer needs to compare it with the grid
+def betterMaxIncreaseKeepingSkyline(self, grid):
+
+    rows, cols = list(map(max, grid)), list(map(max, zip(*grid)))
+    return sum(min(i,j) for i in rows for j in cols) - sum(map(sum, grid))
