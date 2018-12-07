ba3ccb9157fa70f166dedf08149b9c77ba93e927
RioAraki <yueee.li@mail.utoronto.ca>
2018-12-06 21:57:37 -0500

    better 1

--- leetcode/1_TwoSum.py
+++ leetcode/1_TwoSum.py
@@ -1,3 +1,8 @@
+# 1. list is not sorted
+# 2. this would cause index problem when using two pointer method which is the most efficient
+
+# 2018-07 1152ms
+
 class Solution:
     def twoSum(self, nums, target):
         """
@@ -14,3 +19,27 @@ class Solution:
 
 
                         # index error: range(i+1, len(nums)), not range(i, len(nums))
+
+
+# 2018-12-6 52ms
+
+def twoSum(self, nums, target):
+  """
+  :type nums: List[int]
+  :type target: int
+  :rtype: List[int]
+  """
+  
+  i = 0
+  j = len(nums)-1
+  
+  nums = [(idx,i) for idx,i in enumerate(nums)] 
+  nums = sorted(nums, key = lambda x: x[1])
+  
+  while i < j:
+    if nums[i][1] + nums[j][1] > target:
+      j-= 1
+    elif nums[i][1] + nums[j][1] < target:
+      i+=1
+    else:
+      return [nums[i][0],nums[j][0]]
