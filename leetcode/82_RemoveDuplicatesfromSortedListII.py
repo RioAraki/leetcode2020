56ac1307f254e5ed8ad6cfbd0713795c03a88757
RioAraki <yueee.li@mail.utoronto.ca>
2018-12-08 16:01:17 -0500

    done 82, need to redo

---
+++ leetcode/82_RemoveDuplicatesfromSortedListII.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return head
+        
+        dummy = pre = ListNode(0)
+        dummy.next = head
+        
+        # we must need this head because 
+        while head and head.next:
+            if head.val != head.next.val:
+                pre = pre.next
+                head = head.next
+            else:
+                while head.next and head.val == head.next.val:
+                    head = head.next
+                head = head.next
+                pre.next = head
+        
+        
+        return dummy.next
