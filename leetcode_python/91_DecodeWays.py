96c3eb2575958934be907af93b5620029a1a3bf8
RioAraki <yueee.li@mail.utoronto.ca>
2018-12-22 03:08:09 -0500

    done 91, TLE, redo the dp solution

--- leetcode_python/91_DecodeWays.py
+++ leetcode_python/91_DecodeWays.py
@@ -1,3 +1,6 @@
+# 2018-12-22: TLE O(n^2) n = len(s)
+
+
 # idea:
 # find all possible subset's with length 1 or 2. If its 2 and > 26, discard it
 
@@ -29,3 +32,18 @@ def numDecodings(self, s):
                 break
 
     return len(ret) - invalid
+
+# 2018-12-22: DP solution, redo
+def numDecodings(self, s):
+    #dp[i] = dp[i-1] if s[i] != "0"
+    #       +dp[i-2] if "09" < s[i-1:i+1] < "27"
+    if s == "": return 0
+    dp = [0 for x in range(len(s)+1)]
+    dp[0] = 1 # initializer, set to be 1 for the convinence
+    for i in range(len(s)):
+        if s[i] != "0":
+            dp[i+1] += dp[i]
+        if i != 0 and s[i-1:i+1] < "27" and s[i-1:i+1] > "09":  #"01"ways = 0
+            dp[i+1] += dp[i-1]
+    print(dp)
+    return dp[len(s)]
