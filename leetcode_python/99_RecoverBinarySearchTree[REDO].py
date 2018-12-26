111833a92735492236e7fdb946e246357a67aa2d
RioAraki <yueee.li@mail.utoronto.ca>
2018-12-26 17:11:38 -0500

    done 99, need to redo

---
+++ leetcode_python/99_RecoverBinarySearchTree[REDO].py
@@ -0,0 +1,29 @@
+# in order traverse, output should be sorted.
+# use pred and first to record when output is not sorted
+# there should be two times output is not sorted, record the nodes and change their value
+def inorderIter(root):
+    if root:
+        for node in inorderIter(root.left): 
+            yield node
+        yield root        
+        for node in inorderIter(root.right): 
+            yield node
+
+class Solution:
+    # @param root, a tree node
+    # @return a tree node
+    def recoverTree(self, root):
+        pred, first = None, None
+        for node in inorderIter(root):
+            print("node: ",node.val)
+            if pred and pred.val > node.val:
+                print("pred: ",pred.val)
+                if first == None:
+                    first = pred
+                    second = node
+                else:
+                    second = node
+                    break
+            pred = node
+
+        first.val, second.val = second.val, first.val
