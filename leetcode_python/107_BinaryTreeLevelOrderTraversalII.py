bd4b0de4c9b64adbfc2955b16f57c052efe79a4e
RioAraki <yueee.li@mail.utoronto.ca>
2018-12-28 16:52:36 -0500

    done 1-7

---
+++ leetcode_python/107_BinaryTreeLevelOrderTraversalII.py
@@ -0,0 +1,19 @@
+# same as 102
+def levelOrderBottom(self, root):
+    """
+    :type root: TreeNode
+    :rtype: List[List[int]]
+    """
+    def bfs(root, lvl=0):
+        out[lvl].append(root.val)
+        if root.left:
+            bfs(root.left, lvl + 1)
+        if root.right:
+            bfs(root.right, lvl + 1)
+            
+    if not root:
+        return []
+    out = collections.defaultdict(list)
+    bfs(root)
+    
+    return [out[i] for i in out][::-1]
