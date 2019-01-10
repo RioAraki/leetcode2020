8dc88637b1e91c949c81410c82cbabdf0c100877
RioAraki <yueee.li@mail.utoronto.ca>
2019-01-10 00:10:08 -0500

    done 128 pass

---
+++ leetcode_python/128_LongestConsecutiveSequence[Done].py
@@ -0,0 +1,22 @@
+# 2019-01-10: pass
+# use set (hash table) to make the look up process O(1)
+# find the starting point i by checking if i-1 in the list
+
+
+def longestConsecutive(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    set_nums = set(nums)
+    
+    mx = 0
+    for i in nums:
+        # begin of streak
+        if i-1 not in set_nums:
+            streak = 0
+            while i in set_nums:
+                streak += 1
+                i += 1
+            mx = max(mx, streak)
+    return mx
