4625393f8674103566aa0d7a7b6e1a728cb24198
RioAraki <yueee.li@mail.utoronto.ca>
2019-04-09 00:10:59 -0400

    better 1018

--- leetcode_python/1021_RemoveOutermostParentheses.py
+++ leetcode_python/1021_RemoveOutermostParentheses.py
@@ -16,3 +16,14 @@ def removeOuterParentheses(self, S: str) -> str:
                 stack += ")"
                 rightCnt += 1
     return ret
+
+def removeOuterParentheses(self, S: str) -> str:
+    res, opened = "", 0
+    for c in S:
+        # when opened = 0 it would be the outermost parenthesis
+        if c == '(' and opened > 0: res+="("
+        # when opened > 1, this ) would not be the outermost parenthesis
+        if c == ')' and opened > 1: res+=")"
+        # opend += 1 for left, opened -= 1 for right
+        opened += 1 if c == '(' else -1
+    return "".join(res)
