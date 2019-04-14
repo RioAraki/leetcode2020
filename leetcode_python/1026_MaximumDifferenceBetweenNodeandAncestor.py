009078f2cb1c352e5f3b7574374982c9fbff2706
RioAraki <yueee.li@mail.utoronto.ca>
2019-04-14 01:00:59 -0400

    done 1026

--- leetcode_python/1026_MaximumDifferenceBetweenNodeandAncestor.py
+++ leetcode_python/1026_MaximumDifferenceBetweenNodeandAncestor.py
@@ -1,14 +1,5 @@
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-
-def maxAncestorDiff(root: TreeNode) -> int:
-
-
+def maxAncestorDiff(self, root: TreeNode) -> int:
+    result = []
     def DFS(root, path):
         if not root.left and not root.right:
             result.append(path+[root.val])
@@ -16,16 +7,10 @@ def maxAncestorDiff(root: TreeNode) -> int:
             DFS(root.left, path + [root.val])
         if root.right:
             DFS(root.right, path + [root.val])
-
-    result = []
+         
     DFS(root, [])
-    print (result)
-
-if __name__ == "__main__":
-    tree = TreeNode(5)
-    tree.left = TreeNode(3)
-    tree.left.left = TreeNode(1)
-    tree.right = TreeNode(8)
-
-    maxAncestorDiff(tree)
-
+    ret = 0
+    for lst in result:
+        ret = max(ret, max(lst) - min(lst))
+    return ret
+            
