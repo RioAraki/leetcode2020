9501f4018b0c49c04aa04e5e3ee65e10e54cf7d4
RioAraki <yueee.li@mail.utoronto.ca>
2019-06-03 11:29:37 -0400

    better 1071

--- leetcode_python/1071_GreatestCommonDivisorofStrings.py
+++ leetcode_python/1071_GreatestCommonDivisorofStrings.py
@@ -21,3 +21,27 @@ def gcdOfStrings(self, str1, str2):
     else:
         notNull = notNull[0]
         return self.gcdOfStrings(notNull,str2)
+
+
+# better solution
+# 辗转相除,例：
+# str1: ABABAB str2: ABAB
+# str1: ABAB str2: AB
+# str1: AB str2: AB
+# return str1 since they equal
+
+def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        if len(str1) == len(str2):
+            return str1 if str1 == str2 else ''
+        
+        if len(str1) < len(str2):
+            str1, str2 = str2, str1
+        if str1[:len(str2)] == str2:
+            return self.gcdOfStrings(str1[len(str2):], str2)
+        else:
+            return ''
