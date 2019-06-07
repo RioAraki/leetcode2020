11b4a884c6c64a0c3a4ba059773608188cadcb1e
RioAraki <yueee.li@mail.utoronto.ca>
2019-06-07 17:01:55 -0400

    redo 46

--- leetcode_python/46_Permutations.py
+++ leetcode_python/46_Permutations.py
@@ -1,24 +1,21 @@
-def permute(nums, cur = [], ret = []):
-    """
-    :type nums: List[int]
-    :rtype: List[List[int]]
-    """
-    # recursion
-
-    ret = []
-
 
 # The + operator creates a new list in python when 2 lists are combined using it, the original object is not modified.
 # On the other hand, using methods like extend and append, we add the lists in place, ie, the original object is modified
 
-    def dfs(nums, ret, path):
-        print(ret)
-        if not nums:
-            ret.append(path)
-        for i in range(len(nums)):
-            dfs(nums[:i] + nums[i + 1:], ret, path+[nums[i]])
-    dfs(nums, ret, [])
-    return ret
+def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        def dfs(nums, path, res):
+            if not nums:
+                res.append(path)
+            for i in range(len(nums)):
+                
+                # the following commented line is wrong, return would stop code from running after it reaches it once and would not lead to recursion!!!
+                # return dfs(nums[:i] + nums[i+1:], path + [nums[i]], res)
+                
+                dfs(nums[:i] + nums[i+1:], path + [nums[i]], res)
+        res = []
+        dfs(nums, [], res)
+        return res
 
 # dfs with oneline, use enumerate as slicer later
 def oneLinePermute(nums):
