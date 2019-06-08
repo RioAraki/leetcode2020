f819067607fded7fdbd143cea79ae13dc440a600
RioAraki <yueee.li@mail.utoronto.ca>
2019-06-08 13:52:59 -0400

    redo 204

--- leetcode_python/206_ReverseLinkedList[REDO].py
+++ leetcode_python/206_ReverseLinkedList[REDO].py
@@ -4,19 +4,20 @@ class ListNode:
         self.next = None
 
 class Solution:
-    def reverseListIterative(self, head: 'ListNode') -> 'ListNode':
-        
-        prev = None
-        curr = head
-        
-        while curr:
-            tmp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = tmp
-        return prev
-    
 
+# 要理解每一轮翻转链表都要涉及前后两个node，所以做 prev 和 cur 保证每一个loop都能把cur 的 next 放到 prev 上
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        cur = head
+        
+        while cur:
+            tmp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = tmp
+                
+        return prev
+            
     # 1 2 3 4
 
 
