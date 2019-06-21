08697ee059669897ab6bcc0d086349303a1180bf
RioAraki <yueee.li@mail.utoronto.ca>
2019-06-20 23:46:17 -0400

    add 1089

---
+++ leetcode_python/1089_DuplicateZeros.py
@@ -0,0 +1,15 @@
+def duplicateZeros(self, arr: List[int]) -> None:
+    """
+    Do not return anything, modify arr in-place instead.
+    """
+    n = len(arr)
+    i = 0
+    while i < n:
+        if arr[i] == 0:
+            arr.insert(i+1, 0)
+            i += 2
+        else:
+            i += 1
+    
+    while len(arr) > n:
+        del arr[-1]
