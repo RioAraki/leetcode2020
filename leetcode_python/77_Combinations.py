10f1c8f8c84f71011735d03d28920d6326525c64
RioAraki <yueee.li@mail.utoronto.ca>
2019-06-26 23:51:41 -0400

    done 1094

--- leetcode_python/77_Combinations.py
+++ leetcode_python/77_Combinations.py
@@ -24,3 +24,22 @@ def icombine(self, n, k):
         # in the latter loop, i would always be in range(1, c[0]-1)
         combs = [[i] + c for c in combs for i in range(1, c[0] if c else n + 1)]
         print(x, combs)
+
+
+
+
+
+def dfs(start, end, k, path, res):
+
+    if k == 0:
+        # print(path)
+        res.append(path)
+    else:
+        for i in range(start, end+1):
+            dfs(i + 1, end, k - 1, path+[i], res)
+
+
+if __name__ == "__main__":
+    res = []
+    dfs(1,5,3,[],res)
+    print(res)
