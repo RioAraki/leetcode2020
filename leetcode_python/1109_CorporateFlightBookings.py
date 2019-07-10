2d1ca94437893a21f08dc6fd9d17b3ba5c7478f8
RioAraki <yueee.li@mail.utoronto.ca>
2019-07-09 23:45:10 -0400

    more on 1109

--- leetcode_python/1109_CorporateFlightBookings.py
+++ leetcode_python/1109_CorporateFlightBookings.py
@@ -17,13 +17,45 @@ def corpFlightBookings(self, bookings, n):
 
 # better solution: O(n) range caching with cumulative sum
 
-Count of Range Sum 
-Count of Smaller Numbers After Self 
-Falling Squares 
-My Calendar III 
-Range Module 
-Range Sum Query - Mutable 
-Range Sum Query 2D - Mutable 
-Rectangle Area II 
-Reverse Pairs 
-The Skyline Problem
+def corpFlightBookings(self, bookings, n):
+    """
+    :type bookings: List[List[int]]
+    :type n: int
+    :rtype: List[int]
+    """
+    rec = [0 for x in range(n)]
+    ret = [0 for x in range(n)]
+    
+    for start, end, num in bookings:
+        rec[start - 1] += num
+        if end < n:
+            rec[end] -= num    
+    cur = 0
+
+    for i in range(len(rec)):
+        cur += rec[i]
+        ret[i] = cur
+        
+    return ret
+
+
+# 或者可以用线段树的思路解
+
+# runtime: O(n log n)
+
+
+# https://blog.csdn.net/Yaokai_AssultMaster/article/details/79599809
+
+# 可以用线段树思考的相关题目
+# Count of Range Sum 
+# Count of Smaller Numbers After Self 
+# Falling Squares 
+# My Calendar III 
+# Range Module 
+# Range Sum Query - Mutable 
+# Range Sum Query 2D - Mutable 
+# Rectangle Area II 
+# Reverse Pairs 
+# The Skyline Problem
+
+
