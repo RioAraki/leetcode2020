c127dafb063e90c6c9fffec4454b9ea8b0343302
RioAraki <yueee.li@mail.utoronto.ca>
2019-08-31 21:28:43 -0700

    pass 5175

--- leetcode_python/5175_CanMakePalindromefromSubstring.py
+++ leetcode_python/5175_CanMakePalindromefromSubstring.py
@@ -26,3 +26,26 @@ def canMakePaliQueries(s, queries):
             ret.append(True)
 
     return ret
+
+# Pass 5175 with one simple trick, check if substitute > 13, since we can rearrange and there are only 26 letters
+def canMakePaliQueriesPASSED(s, queries):
+    """
+    :type s: str
+    :type queries: List[List[int]]
+    :rtype: List[bool]
+    """
+    ret = []
+
+    for left, right, substitute in queries:
+        if substitute > 13:
+            ret.append(True)
+        else:
+            tmp = collections.Counter(s[left:right + 1])
+            odds = sum([x % 2 == 1 for x in tmp.values()])
+
+            if odds // 2 <= substitute:
+                ret.append(True)
+            else:
+                ret.append(False)
+
+    return ret
