364a237e9808bb36396f3a406f5dda998c91612c
RioAraki <yueee.li@mail.utoronto.ca>
2019-09-03 14:29:37 -0700

    new 24

--- leetcode_python/24_SwapNodesinPairs.py
+++ leetcode_python/24_SwapNodesinPairs.py
@@ -18,3 +18,27 @@ class Solution(object):
             pre.next, b.next, a.next = b, a, b.next
             pre = a
         return self.next
+
+
+# 2019-09-03 redo
+# understand the link's relation is important
+
+def swapPairs(head):
+	"""
+	:type head: ListNode
+	:rtype: ListNode
+	"""
+	dummy = prev = ListNode(0)
+	prev.next = head
+	while prev.next and prev.next.next:
+	    a = prev.next
+	    b = prev.next.next
+	    
+	    prev.next = b
+	    a.next = a.next.next
+	    b.next = a
+	    prev = prev.next.next
+	    
+	return dummy.next
+
+
