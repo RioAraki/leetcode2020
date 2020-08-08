79dd0ced3110252d140d221eb2483937c2b666bb
Yue Li <yueee.li@mail.utoronto.ca>
2020-08-08 00:26:16 -0700

    done vertical order travseral of binary tree

--- AugustLeetcodeChallenge2020/findAllDuplicatesInAnArray.java
+++ AugustLeetcodeChallenge2020/findAllDuplicatesInAnArray.java
@@ -1,3 +1,6 @@
+// this solution uses the property that search in hashset is O(1), so overall runtime is O(n)
+// but it still requires extra O(n) space
+
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
         HashSet<Integer> nonDuplicate = new HashSet<Integer>();
@@ -14,3 +17,8 @@ class Solution {
         return result;
     }
 }
+
+// rearrange number
+// We can do it because we know the list should be consecutive number start from 1
+// swap until index matches number or duplicate
+// after finalize rearrange, check which index does not match the number
