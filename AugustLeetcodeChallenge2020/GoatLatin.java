2c8481c76811e282a1a1b7e72c80d88940fa4684
Yue Li <yueee.li@mail.utoronto.ca>
2020-08-20 21:24:56 -0700

    done reorder list

--- AugustLeetcodeChallenge2020/GoatLatin.java
+++ AugustLeetcodeChallenge2020/GoatLatin.java
@@ -29,3 +29,16 @@ class Solution {
         return goatWord;
     }
 }
+
+public String toGoatLatin(String S) {
+    Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+    String res = "";
+    int i = 0, j = 0;
+    for (String w : S.split("\\s")) {
+        res += ' ' + (vowels.contains(w.charAt(0)) ? w : w.substring(1) + w.charAt(0)) + "ma";
+        for (j = 0, ++i; j < i; ++j) {
+            res += "a";
+        }
+    };
+    return res.substring(1);
+}
