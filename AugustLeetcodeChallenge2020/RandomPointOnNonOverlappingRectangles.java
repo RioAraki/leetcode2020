37418ac242150f5970178339584cc1226e3fc8f5
Yue Li <yueee.li@mail.utoronto.ca>
2020-08-22 14:25:38 -0700

    more explain

--- AugustLeetcodeChallenge2020/RandomPointOnNonOverlappingRectangles.java
+++ AugustLeetcodeChallenge2020/RandomPointOnNonOverlappingRectangles.java
@@ -1,3 +1,10 @@
+// 很有启发，说几点:
+// 1. 因为不同rect面积不同，涵盖的integer point个数不同，所以不能随机选rect，大的rect选中比例大，要根据面积决定比例
+// 2. 1*1 面积的 rect 能容纳 4 个点，2*2面积的rect能容纳9个点，要按照点的比例算
+// 3. random pick number 只需要1次就够了，这一次已经包含了它会在哪个rect里，以及是rect的哪个点
+
+
+
 class Solution {
     
     private int[][] rects;
