dc372c9f4ef8555a65ec4a4b5d7205ea3711383d
Yue Li <yueee.li@mail.utoronto.ca>
2021-06-02 20:20:02 -0700

    redo 11

--- leetcode_python/11_ContainerWithMostWater.py
+++ leetcode_python/11_ContainerWithMostWater.py
@@ -1,20 +1,19 @@
-class Solution:
-    def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        # idea: two pointer search
+def maxArea(height):
+    """
+    :type height: List[int]
+    :rtype: int
+    """
+    # idea: two pointer search
 
-        water = 0
-        i, j = 0, len(height) - 1
-        while (i != j):
-            water = max(water, min(height[i], height[j]) * (j - i))
-            if height[i] > height[j]:
-                j -= 1
-            else:
-                i += 1
-        return water
+    water = 0
+    i, j = 0, len(height) - 1
+    while (i != j):
+        water = max(water, min(height[i], height[j]) * (j - i))
+        if height[i] > height[j]:
+            j -= 1
+        else:
+            i += 1
+    return water
 
 #     def maxArea(self, height):
 #         """
@@ -33,3 +32,17 @@ class Solution:
 # Error 1: logic error: calculate the height wrong, should pick the minimal one not the difference
 # Error 2: TLE
 # Error 3: wrong index, forgot len(list)-1 is the real last element
+
+
+# 2021-06-02
+
+def maxArea2(height: List[int]) -> int:
+    left, right = 0, len(height)-1
+    maxArea = 0
+    while left < right:
+        maxArea = max(maxArea, (right-left) * min(height[left], height[right]))
+        if height[left] <= height[right]:
+            left += 1
+        else:
+            right -= 1
+    return maxArea
